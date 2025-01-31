4f57e277cf502474bab7be9fb6b46dfe18fd7ad3
mocos <130593875+MoXcz@users.noreply.github.com>
2025-01-31 16:09:51 -0500

    Update Csv entries for more descriptive names

--- src/log.py
+++ src/log.py
@@ -5,7 +5,9 @@ import csv
 def checkCsvFile(file):
     if os.stat(file.name).st_size == 0:
         csv_writer = csv.writer(file)
-        csv_writer.writerow(["Name", "Age"])
+        csv_writer.writerow(["Work Time", "Break Time"])
+    else:
+        csvParser(file.name)
 
 
 def csvParser(filename):
