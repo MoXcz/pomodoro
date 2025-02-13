ca3d659b1bc60c4f400fe5d9d5170cfee09e9037
mocos <130593875+MoXcz@users.noreply.github.com>
2025-02-12 19:08:24 -0500

    Refactor config file and dir and log file definition

--- src/config.py
+++ src/config.py
@@ -7,39 +7,60 @@ import configparser
 class Configuration:
     def __init__(self, config_dir=".config"):
         self.config_dir = config_dir
-        self.set_config_dir()  # set configuration inside /home/user/ (~)
-        self.config_file = ""
-        self.log_file = ""
+        self.set_config_directory()  # set configuration inside /home/user/config_dir (~)
+        self.log_file = os.path.join(self.config_dir, "sessions.csv")
+        self.config_file = os.path.join(self.config_dir, "config.ini")
+        self.initialize_configuration()
+        self.params = self.load_config()["arguments"]
+
+    def initialize_configuration(self):
+        # TODO: If config_dir exists -> read from config.ini and set config_file
+        # and log_file from there
+        # Check if the configuration directory exists
+        if os.path.exists(self.config_dir):
+            return
+
+        while True:
+            user_input = (
+                input(
+                    f"Do you want to create config.ini at {self.config_dir}? [yes or no]: "
+                )
+                .strip()
+                .lower()
+            )
+
+            if user_input in {"yes", "y"}:
+                self.create_config_file_and_dir()
+                self.create_log_file()
+                self.populate_config_file()
+                break
+            elif user_input in {"no", "n"}:
+                print("Nothing happened, exiting")
+                exit(1)
 
     def create_log_file(self):
-        if not self.config_dir:
+        if not self.config_dir or os.path.exists(self.log_file):
             return
-        self.log_file = os.path.join(self.config_dir, "sessions.csv")
-        if not os.path.exists(self.log_file):
-            print(f"Created: {self.log_file}")
-            open(self.log_file, "x")
+
+        print(f"Creating log file: {self.log_file}")
+        open(self.log_file, "x")
 
     def create_config_file_and_dir(self):
-        if not self.config_dir:
+        if not self.config_dir or os.path.exists(self.log_file):
             return
-        pomodoro_dir = os.path.join(self.config_dir, "pomodoro")
-        if not os.path.exists(pomodoro_dir):
-            print(f"Created: {pomodoro_dir}")
-            os.mkdir(pomodoro_dir)
-        self.config_dir = pomodoro_dir
 
-        pomodoro_config = os.path.join(pomodoro_dir, "config.ini")
-        if not os.path.exists(pomodoro_config):
-            print(f"Created: {pomodoro_config}")
-            open(pomodoro_config, "x")
-        self.config_file = pomodoro_config
+        print(f"Creating config directory: {self.config_dir}")
+        os.mkdir(self.config_dir)
 
-    def parse_config_file(self):
-        if not self.config_file:
-            return
-        config = open(self.config_file, "r").read()
+        if not os.path.exists(self.config_file):
+            print(f"Creating config file: {self.config_file}")
+            open(self.config_file, "x")
+
+    def load_config(self):
         parser = configparser.ConfigParser()
-        parser.read_string(config)
+        if not os.path.exists(self.config_file):
+            return parser
+        parser.read(self.config_file)
         return parser
 
     def populate_config_file(self):
@@ -61,12 +82,10 @@ log = 1
         file = open(self.config_file, "w")
         file.write(sensible_default)
 
-    def set_config_dir(self):
+    def set_config_directory(self):
         home = os.path.expanduser("~")
         config_dir = os.path.expandvars("$XDG_CONFIG_HOME")
         if not config_dir or self.config_dir != ".config":
             config_dir = os.path.join(home, self.config_dir)
-            print(
-                f"XDG_CONFIG_HOME env var not set or \nconfiguration directory specified, config_dir set to: {config_dir}"
-            )
+        config_dir = os.path.join(config_dir, "pomodoro")
         self.config_dir = config_dir
